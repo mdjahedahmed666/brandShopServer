ce34f33f4f4d0e7a33ea4c4e989482dbaa20f045
Jahed <mdjahedahmed666@gmail.com>
2023-10-20 11:34:38 +0100

    user post and get api is created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,6 +28,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const productCollection = client.db("productDB").collection("products");
+    const userCollection = client.db("productDB").collection("users");
 
     app.get("/products/:brandName", async (req, res) => {
       const brandName = req.params.brandName;
@@ -35,19 +36,29 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-
-    // app.get("/products/bybrand/:brandName", async (req, res) => {
-    //   const brandName = req.params.brandName;
-    //     const cursor = productCollection.find({ brandName: brandName });
-    //     const result = await cursor.toArray();
-    //     res.send(result);
-    // })
 
     app.post("/products", async (req,res) =>{
         const newProduct = req.body;
         console.log(newProduct);
         const result = await productCollection.insertOne(newProduct);
+        res.send(result);
+    })
+    app.post("/users", async (req,res) =>{
+        const newProduct = req.body;
+        console.log(newProduct);
+        const result = await userCollection.insertOne(newProduct);
         res.send(result);
     })
 
